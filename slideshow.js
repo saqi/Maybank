9bc88b956442da92fc9ebf27419ced0097fd325d
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2018-10-18 21:34:50 +0100

    SlideShow should work now ;-)

--- slideshow.js
+++ slideshow.js
@@ -2,15 +2,15 @@
 
 var
   index = 0
-, timeNextSlide = 2 // in seconds
+, timeNextSlide = 7 // in seconds
 , slides = $('#slideshow figure')
 , prev = $('#prev')
 , next = $('#next')
-, timerID = null
+, paused = false
+, paused10 = false
 ;
 
 function showSlides() {
-  console.log(slides.length, index);
   for (var i=0; i<slides.length; i++) {
     slides[i].style.display = 'none';
   }
@@ -19,15 +19,28 @@ function showSlides() {
   if (index < 0)
     index = slides.length - 1;
   slides[index].style.display = 'block';
+
+  setTimeout(function () {
+    if (false === paused) {
+      ++index;
+      showSlides();
+    }
+  }, timeNextSlide * 1000);
+};
+
+function pauseFor10seconds() {
+  if (paused10 === true) return;
+  paused10 = true;
+  setTimeout(function () {
+    paused = false;
+    ++index;
+    showSlides();
+    paused10 = false;
+  }, 10000);
 };
 
 $( window ).on('load', function () {
   showSlides();
-  next.click(function () { index++; showSlides(); });
-  prev.click(function () { index--; showSlides(); });
-
-  window.setTimeout(function () {
-    index = index + 1;
-    showSlides();
-  }, 1000);
+  next.click(function () { paused = true; index++; showSlides(); pauseFor10seconds(); });
+  prev.click(function () { paused = true; index--; showSlides(); pauseFor10seconds(); });
 });
